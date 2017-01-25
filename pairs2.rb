43f45dee3d1e561e4fcb48879e16832f1c4bb80b
Daniel Parks <dparks715@yahoo.com>
2017-01-25 18:32:30 -0500

    verified class was array

--- pairs2.rb
+++ pairs2.rb
@@ -5,6 +5,11 @@ def create_pairs(names)
 	#assigns to pair_array
     pair_array = names.shuffle.each_slice(2).to_a
        print pair_array 
+       if pair_array.class == Array
+       	puts "True"
+       else
+       	puts "False"
+       end
 	end
 
 create_pairs(['Dan', 'Marv', 'Tim', 'Shirley'])
