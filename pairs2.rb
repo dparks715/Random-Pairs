f96d7b5b18777dedd42ed26231de2dd9732a5872
Daniel Parks <dparks715@yahoo.com>
2017-01-25 18:26:25 -0500

    shuffle and slice in one line

--- pairs2.rb
+++ pairs2.rb
@@ -1,4 +1,8 @@
 def create_pairs(names)
-    pair_array = names.each_slice(2).to_a
+	#works for even numbered tests
+	#takes inputted names, splits in twos,
+	#shuffles and converts to array 
+	#assigns to pair_array
+    pair_array = names.shuffle.each_slice(2).to_a
         
 	end
